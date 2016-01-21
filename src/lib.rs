1f9b485ba5cdf55b59f092ba607507809b41dce0
jer <foobar>
2016-01-20 19:49:19 -0700

    It's working

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,32 @@
 extern crate git2;
 
+use std::path::Path;
 use git2::{Repository, Signature, Error};
 
-struct User {
-    email: String,
-    name: String,
-    time: String
+pub fn add<P: AsRef<Path>>(repo: &str, files: &[P]) -> Result<(), Error> {
+    let repo = try!(Repository::open(repo));
+    let mut index = try!(repo.index());
+
+    for path in files {
+        let _ = try!(index.add_path(path.as_ref()));
+    }
+
+    index.write()
 }
 
-pub fn add_and_commit(repo: &str, name: &str, email: &str, message: &str, files: &[&str]) -> Result<(), Error> {
+pub fn commit(repo: &str, name: &str, email: &str, message: &str) -> Result<(), Error> {
     let signature = try!(Signature::now(name, email));
     let update_ref = Some("HEAD");
 
     let repo = try!(Repository::open(repo));
 
     let oid = try!(repo.refname_to_id("HEAD"));
-    let commit = try!(repo.find_commit(oid));
+    let parent_commit = try!(repo.find_commit(oid));
+    let parents = vec![&parent_commit];
 
-    let tree = try!(commit.tree());
-    let parents = vec![&commit];
+    let mut index = try!(repo.index());
+    let tree_oid = try!(index.write_tree());
+    let tree = try!(repo.find_tree(tree_oid));
 
     repo
         .commit(update_ref, &signature, &signature, message, &tree, &parents)
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,78 @@
 extern crate git2;
 extern crate git2_commit;
+extern crate docopt;
+extern crate rustc_serialize;
 
-use git2_commit::add_and_commit;
+use docopt::Docopt;
+use git2::Error;
+
+const USAGE: &'static str = "
+git2-commit
+
+Usage:
+  git2-commit add [--repository=<path>] <file>...
+  git2-commit commit [--repository=<path>] <name> <email> <message>
+
+Options:
+  -r <path>, --repository=<path>  Path to the repository's working directory [default: .]
+  -h, --help                      Show this screen.
+";
+
+
+#[derive(Debug,RustcDecodable)]
+struct Args {
+    arg_file: Vec<String>,
+
+    arg_name: String,
+    arg_email: String,
+    arg_message: String,
+
+    flag_repository: String,
+    cmd_add: bool,
+    cmd_commit: bool,
+}
+
+fn git_add(args: &Args) -> Result<(), Error> {
+    let repo = &args.flag_repository;
+    let files = &args.arg_file;
+
+    git2_commit::add(repo, files)
+}
+
+fn git_commit(args: &Args) -> Result<(), Error> {
+    let repo = &args.flag_repository;
+
+    let name = &args.arg_name;
+    let email = &args.arg_email;
+    let message = &args.arg_message;
+
+    git2_commit::commit(repo, name, email, message)
+}
+
+fn run(args: &Args) -> Result<(), Error> {
+
+    if args.cmd_add {
+        return git_add(args);
+    }
+
+    if args.cmd_commit {
+        return git_commit(args);
+    }
+
+    Err(Error::from_str("Unknown command"))
+}
 
 fn main() {
-    add_and_commit(".", "jer", "foobar", "first commit", &[]).unwrap();
+    //add(".", &["foo.txt"]).unwrap();
+    //commit(".", "jer", "foobar", "next commit").unwrap();
+
+    let args : Args = Docopt::new(USAGE)
+        .and_then(|d| d.decode())
+        .unwrap_or_else(|e| e.exit());
+
+    match run(&args) {
+        Ok(()) => {}
+        Err(e) => println!("error: {}", e),
+
+    }
 }
