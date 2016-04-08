7432f3cfc97419578786bb769e5936cb511a17cb
Jan-Erik Rediger <janerik@fnordig.de>
2016-04-08 18:28:12 +0200

    chore: Enable clippy for nightly builds

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+#![cfg_attr(feature = "clippy", allow(unstable_features))]
+#![cfg_attr(feature = "clippy", feature(plugin))]
+#![cfg_attr(feature = "clippy", plugin(clippy))]
+
 extern crate git2;
 
 use std::path::Path;
